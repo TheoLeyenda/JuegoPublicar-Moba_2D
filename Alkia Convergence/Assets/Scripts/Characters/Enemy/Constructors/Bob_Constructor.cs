e2400b366f668860bad79355205c9e0666ea3079
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2020-06-18 03:47:01 -0300

    Estructura de clases

---
+++ Alkia Convergence/Assets/Scripts/Characters/Enemy/Constructors/Bob_Constructor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bob_Constructor : ConstructorClass
+{
+    // Start is called before the first frame update
+    [Header("Datos Generales que se suman a tus skills al pasar de nivel")]
+    protected int indexDataLevelUpBob;
+    [System.Serializable]
+    public class DataLevelUpBob
+    {
+        //Aqui van las estadisticas que se subiran del nivel de bob
+        //Estas estadisticas no se asignas sino que se suman a las que ya tenes
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Alkia Convergence/Assets/Scripts/Ejemplo Delegates/UI_Manager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace ExampleDelegate
+{
+    public class UI_Manager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public Text textScore;
+        void Start()
+        {
+            textScore.text = "Score: ";
+        }
+        void OnEnable()
+        {
+            Player.OnKillEnemy += CheckTextScore;
+        }
+        void OnDisable()
+        {
+            Player.OnKillEnemy -= CheckTextScore;
+        }
+
+        void CheckTextScore(Player p)
+        {
+            textScore.text = textScore.text + p.scoreForKillEnemy;
+        }
+    }
+}
